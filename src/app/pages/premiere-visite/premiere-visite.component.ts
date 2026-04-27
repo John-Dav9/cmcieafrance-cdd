3747bdc40688e1ecb874a7c3481c2bb774493191
John-Dav9 <tchomguijohn@gmail.com>
2026-04-28 01:14:51 +0200

    fix: prochain culte dynamique sur premiere-visite, retrait flyer du modal

--- src/app/pages/premiere-visite/premiere-visite.component.ts
+++ src/app/pages/premiere-visite/premiere-visite.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { PageContentService } from '../../core/services/page-content.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-premiere-visite',
@@ -8,11 +10,16 @@ import { PageContentService } from '../../core/services/page-content.service';
 })
 export class PremiereVisiteComponent implements OnInit {
   c: any = {};
+  nextCulte: any = null;
 
-  constructor(private pageContent: PageContentService) {}
+  constructor(private pageContent: PageContentService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.pageContent.getPage('premiere-visite').subscribe((res) => { if (res) this.c = res; });
+    this.http.get<any>(`${environment.apiBase}/settings/next-culte`).subscribe({
+      next: (data) => { if (data?.date) this.nextCulte = data; },
+      error: () => {},
+    });
   }
 
   get heroTitle()      { return this.c.heroTitle      || 'PREMIÈRE VISITE'; }
