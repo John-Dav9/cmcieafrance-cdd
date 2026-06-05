3e689f32f268b6dd8944c81e9e3edf495196627a
John-Dav9 <tchomguijohn@gmail.com>
2026-06-05 13:23:24 +0200

    fix: correct jitsi set timeout

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -2,13 +2,16 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { MemberAuthService } from '../services/member-auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private auth: AuthService,
+    private memberAuth: MemberAuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.auth.getToken();
+    const token = this.memberAuth.getToken() || this.auth.getToken();
     if (!token) return next.handle(req);
 
     return next.handle(req.clone({
